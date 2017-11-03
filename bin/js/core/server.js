10c012e7727d090d20525c8e7f978d164652d5de
DESKTOP-KNVO7ED\lpc <aidimie@163>
2017-11-03 22:26:20 +0800

    没办法，将回调和socket合并再一起试一下吧！！！

--- bin/js/core/server.js
+++ bin/js/core/server.js
@@ -1,4 +1,3 @@
-/// <reference path="../libs/proto"/>
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
@@ -9,68 +8,17 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var ProtoBuf = dcodeIO.ProtoBuf;
 var Server = /** @class */ (function (_super) {
     __extends(Server, _super);
     function Server() {
         var _this = _super.call(this) || this;
+        _this.num = 100;
         _this.socket = new Laya.Socket();
         _this._protoIDs = ProtoIDs.getMap();
         _this.protoBuf = Laya.Browser.window.protobuf;
         _this._protoBuilderUserMap = _this.protoBuf.load("../laya/proto/user.proto");
         return _this;
     }
-    Server.prototype.protoEncode = function (name, data) {
-        if (!data || !name) {
-            return;
-        }
-        var protoBuilderMap = this._protoBuilderUserMap;
-        var buffer;
-        protoBuilderMap.then(function (root) {
-            var AwesomeMessage = root.lookup(name);
-            //let AwesomeMessage = root.lookup("user.UserInfoRequest");
-            //let message = AwesomeMessage.create({uid: 123});
-            var message = AwesomeMessage.create(data);
-            // Verify the message if necessary (i.e. when possibly incomplete or invalid)
-            var errMsg = AwesomeMessage.verify(message);
-            if (errMsg) {
-                console.log(errMsg);
-                return;
-                //throw Error(errMsg);
-            }
-            // Encode a message to an Uint8Array (browser) or Buffer (node)
-            buffer = AwesomeMessage.encode(message).finish();
-        });
-        if (!buffer) {
-            console.log("123");
-        }
-        return buffer;
-    };
-    Server.prototype.protoDecode = function (data) {
-        if (!data) {
-            return;
-        }
-        var protoBuilderMap = this._protoBuilderUserMap;
-        var name = "user.UserInfoRequest";
-        var decodeData;
-        protoBuilderMap.then(function (root) {
-            var AwesomeMessage = root.lookup(name);
-            decodeData = AwesomeMessage.decode(data);
-        });
-        if (decodeData) {
-            console.log(decodeData);
-        }
-        return decodeData;
-    };
-    Server.prototype.testProto = function () {
-        var buffer = this.protoEncode("user.UserInfoRequest", { uid: 123 });
-        if (buffer) {
-            var decodeData = this.protoDecode(buffer);
-            if (decodeData) {
-                console.log(decodeData);
-            }
-        }
-    };
     Server.prototype.connect = function (addr, port) {
         this.socket.connectByUrl(addr + ":" + port);
         this.socket.on(Laya.Event.OPEN, this, this.onSocketOpen);
@@ -79,12 +27,17 @@ var Server = /** @class */ (function (_super) {
         this.socket.on(Laya.Event.ERROR, this, this.onConnectError);
     };
     Server.prototype.onSocketOpen = function () {
+        console.log("socket open");
     };
     Server.prototype.onSocketClose = function () {
+        console.log("socket close");
     };
-    Server.prototype.onMessageReveived = function (bytes) {
-        var len = bytes.readUnsignedShort();
-        var nameId = bytes.readUnsignedShort();
+    Server.prototype.onMessageReveived = function (data) {
+        var _this = this;
+        var bytes = new Laya.Byte();
+        bytes.writeArrayBuffer(data);
+        var len = bytes.getUint16();
+        var nameId = bytes.getUint16();
         switch (nameId) {
             case 0://登录成功
                 console.log("登录成功");
@@ -106,40 +59,56 @@ var Server = /** @class */ (function (_super) {
                 var index = name_1.indexOf('.');
                 var module = name_1.substring(0, index);
                 var action = name_1.substring(index + 1);
-                var body = new Laya.Byte();
-                bytes.readBytes(body);
+                var protoBuilderMap = void 0;
                 if (module == "user") {
-                    var Message = this._protoBuilderUserMap.lookup(name_1);
-                    var message = this._protoBuilderUserMap.decode(body.buffer);
-                    this.event(name_1, message);
+                    protoBuilderMap = this._protoBuilderUserMap;
                 }
                 else {
-                    var Message = this._protoBuilderGameMap.lookup(name_1);
-                    var message = this._protoBuilderGameMap.decode(body.buffer);
-                    this.event(name_1, message);
+                    protoBuilderMap = this._protoBuilderGameMap;
                 }
+                protoBuilderMap.then(function (root) {
+                    var AwesomeMessage = root.lookup(name_1);
+                    var decodeData = AwesomeMessage.decode(bytes.buffer);
+                    _this.event(name_1, decodeData);
+                });
         }
     };
     Server.prototype.onConnectError = function (e) {
+        console.log("connect error");
     };
     Server.prototype.sendData = function (name, data, cb) {
+        var _this = this;
         if (cb === void 0) { cb = null; }
-        var head = name.substring(0, name.indexOf('.'));
-        var Message;
-        if (head == "user") {
-            Message = this._protoBuilderUserMap.build(name);
+        var index = name.indexOf('.');
+        var module = name.substring(0, index);
+        if (!data || !name) {
+            return;
+        }
+        var protoBuilderMap;
+        if (module == "user") {
+            protoBuilderMap = this._protoBuilderUserMap;
         }
         else {
-            Message = this._protoBuilderGameMap.build(name);
+            protoBuilderMap = this._protoBuilderGameMap;
         }
-        var message = new Message(data);
-        var body = new Laya.Byte(message.encodeAB());
-        var pkg = new Laya.Byte();
-        pkg.writeUint16(body.length + 2);
-        pkg.writeUint16(this._protoIDs[name]);
-        pkg.writeArrayBuffer(body);
-        this.socket.send(pkg);
-        this.socket.flush();
+        protoBuilderMap.then(function (root) {
+            var AwesomeMessage = root.lookup(name);
+            var message = AwesomeMessage.create(data);
+            var errMsg = AwesomeMessage.verify(message);
+            if (errMsg) {
+                console.log(errMsg);
+                return;
+                //throw Error(errMsg);
+            }
+            var buffer = AwesomeMessage.encode(message).finish();
+            var pkg = new Laya.Byte();
+            pkg.endian = Laya.Byte.BIG_ENDIAN;
+            pkg.writeUint16(buffer.length + 2);
+            pkg.writeUint16(_this._protoIDs[name]);
+            pkg.writeArrayBuffer(buffer);
+            _this.socket.send(pkg.buffer);
+            _this.socket.flush();
+        });
     };
     return Server;
 }(Laya.EventDispatcher));
--- src/core/server.ts
+++ src/core/server.ts
@@ -1,6 +1,3 @@
-
-//import ProtoBuf = dcodeIO.ProtoBuf;
-
 class Server extends Laya.EventDispatcher{
     private socket:Laya.Socket;
     private protoBuf;
@@ -8,75 +5,17 @@ class Server extends Laya.EventDispatcher{
     private _protoIDs;
     private _protoBuilderUserMap;
     private _protoBuilderGameMap;
+
+    private num:number;
+
     constructor(){
         super();
+        this.num = 100;
         this.socket = new Laya.Socket();
         this._protoIDs = ProtoIDs.getMap();
         this.protoBuf =  Laya.Browser.window.protobuf;
         this._protoBuilderUserMap = this.protoBuf.load("../laya/proto/user.proto");
     }
-
-    private protoEncode(name:string, data:any):any{
-        if(!data || !name){
-            return;
-        }
-
-        let protoBuilderMap = this._protoBuilderUserMap;
-        let buffer;
-
-        protoBuilderMap.then( (root)=>{
-            let AwesomeMessage = root.lookup(name);
-            //let AwesomeMessage = root.lookup("user.UserInfoRequest");
-            //let message = AwesomeMessage.create({uid: 123});
-
-            let message = AwesomeMessage.create(data);
-            // Verify the message if necessary (i.e. when possibly incomplete or invalid)
-            let errMsg = AwesomeMessage.verify(message);
-            if (errMsg){
-                console.log(errMsg);
-                return;
-                //throw Error(errMsg);
-            }
-            // Encode a message to an Uint8Array (browser) or Buffer (node)
-            buffer = AwesomeMessage.encode(message).finish();
-        })
-
-        if(!buffer){
-            console.log("123");
-        }
-        return buffer;
-    }
-
-    private protoDecode(data:any):any{
-        if(!data){
-            return;
-        }
-
-        let protoBuilderMap = this._protoBuilderUserMap;
-        let name = "user.UserInfoRequest";
-        let decodeData;
-
-        protoBuilderMap.then( (root)=>{
-           let AwesomeMessage = root.lookup(name);
-           decodeData = AwesomeMessage.decode(data); 
-        })
-
-        if(decodeData){
-            console.log(decodeData);
-        }
-        return decodeData;
-    }
-
-    public testProto(){
-        let buffer = this.protoEncode("user.UserInfoRequest", {uid: 123});
-        if(buffer){
-            let decodeData = this.protoDecode(buffer);
-            if(decodeData){
-                console.log(decodeData);
-            }
-        }
-    }
-
 
     public connect(addr:string, port:number){
         this.socket.connectByUrl(addr + ":" + port);
@@ -87,28 +26,30 @@ class Server extends Laya.EventDispatcher{
     }
 
     public onSocketOpen(){
-        
+        console.log("socket open");
     }
 
     public onSocketClose(){
-
+        console.log("socket close");
     }
 
-    public onMessageReveived(bytes: any){
+    public onMessageReveived(data: any){
+        let bytes:Laya.Byte = new Laya.Byte();
+        bytes.writeArrayBuffer(data);
 
-		let len: number = bytes.readUnsignedShort();
-		let nameId = bytes.readUnsignedShort();
+		let len: number = bytes.getUint16();
+		let nameId = bytes.getUint16();
 		
 		switch (nameId) {
-			case 0://登录成功
+			case 0:
                 console.log("登录成功");
                 this.event("LOGIN_SUCCESS");
 				break;
-			case 1://登录失败
+			case 1:
                 console.log("登录失败");
                 this.event("LOGIN_FAILED");
 				break;
-			case 2://心跳包
+			case 2:
 				console.log("收到心跳");
 				break;
 			default:
@@ -121,74 +62,59 @@ class Server extends Laya.EventDispatcher{
 				let index: number = name.indexOf('.');
 				let module: string = name.substring(0, index);
 				let action: string = name.substring(index + 1);
+                let protoBuilderMap;
 
-                let body: Laya.Byte = new Laya.Byte();
-				bytes.readBytes(body);
-
-                if ( module == "user"){
-                    let Message = this._protoBuilderUserMap.lookup(name);
-                    let message = this._protoBuilderUserMap.decode(body.buffer);
-                    this.event(name, message);
+                if(module == "user"){
+                    protoBuilderMap = this._protoBuilderUserMap;
+                } else {
+                    protoBuilderMap = this._protoBuilderGameMap;
                 }
-                else{
-                    let Message = this._protoBuilderGameMap.lookup(name);
-                    let message = this._protoBuilderGameMap.decode(body.buffer);
-                    this.event(name, message);
-                }
-
-
-				
-
-                // let AwesomeMessage = root.lookup("awesomepackage.AwesomeMessage");
-                // let message = AwesomeMessage.create({
-                //     awesomeField: "AwesomeString"
-                // });
-
-                // // Verify the message if necessary (i.e. when possibly incomplete or invalid)
-                // let errMsg = AwesomeMessage.verify(message);
-                // if (errMsg)
-                //     throw Error(errMsg);
-
-                // // Encode a message to an Uint8Array (browser) or Buffer (node)
-                // let buffer = AwesomeMessage.encode(message).finish();
-
-                // // ... do something with buffer
-                // // Or, encode a plain object  也可以用这种方式创建这个数据然后编码
-                // let buffer = AwesomeMessage.encode({
-                //     awesomeField: "AwesomeString"
-                // }).finish();
-
-                // // ... do something with buffer
-                // // Decode an Uint8Array (browser) or Buffer (node) to a message   解码处理
-                // let message = AwesomeMessage.decode(buffer); 
+                protoBuilderMap.then( (root)=>{
+                    let AwesomeMessage = root.lookup(name);
+                    let decodeData = AwesomeMessage.decode(bytes.buffer);
+                    this.event(name, decodeData);
+                })
         }
     }
 
     public onConnectError(e: Event){
-
+        console.log("connect error");
     }
 
     public sendData(name: string, data: any, cb: Function = null): void {
-		let head: string = name.substring(0, name.indexOf('.'));
-        let Message;
-        if(head == "user"){
-            Message = this._protoBuilderUserMap.build(name);
+        let index: number = name.indexOf('.');
+        let module: string = name.substring(0, index);
+		if(!data || !name){
+            return;
         }
-        else{
-            Message = this._protoBuilderGameMap.build(name);
+
+        let protoBuilderMap;
+        if(module == "user"){
+            protoBuilderMap = this._protoBuilderUserMap;
+        } else {
+            protoBuilderMap = this._protoBuilderGameMap;
         }
-		 
-		let message = new Message(data);
-		
-		let body: Laya.Byte= new Laya.Byte(message.encodeAB());
 
-		let pkg: Laya.Byte = new Laya.Byte();
+        protoBuilderMap.then( (root)=>{
+            let AwesomeMessage = root.lookup(name);
+            let message = AwesomeMessage.create(data);
+            let errMsg = AwesomeMessage.verify(message);
+            if (errMsg){
+                console.log(errMsg);
+                return;
+                //throw Error(errMsg);
+            }
+            let buffer = AwesomeMessage.encode(message).finish();
 
-        pkg.writeUint16(body.length + 2);
-        pkg.writeUint16(this._protoIDs[name]);
-        pkg.writeArrayBuffer(body);
+            let pkg: Laya.Byte = new Laya.Byte();
 
-        this.socket.send(pkg);
-		this.socket.flush();
+            pkg.endian = Laya.Byte.BIG_ENDIAN;
+            pkg.writeUint16(buffer.length + 2);
+            pkg.writeUint16(this._protoIDs[name]);
+            pkg.writeArrayBuffer(buffer);
+
+            this.socket.send(pkg.buffer);
+            this.socket.flush();
+        })
 	}
 }
